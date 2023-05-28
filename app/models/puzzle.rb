089a4054f0f9c0957737630875aa665a5ac5b010
daisuke saito <dsaito@saitoudaisukenoMacBook-Pro.local>
2023-05-28 12:21:45 +0900

    remove puzzle file temporarily

--- app/models/puzzle.rb
+++
@@ -1,8 +0,0 @@
-class puzzle < ApplicationRecord
-  serialize :yes_no_examples, Array
-  serialize :score_examples, Array
-  validates :content, length: {maximum: 400}, presence: true
-  validates :answer, length: {maximum: 400}, presence: true
-  validates :yes_no_examples, length: {maximum: 400}
-  validates :score_examples, length: {maximum: 400}
-end
